76fe9278119ca61fdf1735984a4b1fb69f5ae700
Teerasej <teerasej@gmail.com>
2020-11-29 20:22:49 +0700

    Add button to create new item

--- src/App.js
+++ src/App.js
@@ -2,33 +2,44 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HeaderBar from './components/HeaderBar';
-import {Layout, Menu} from 'antd';
+import { Layout, Row, Col, Button, Divider } from 'antd';
 import ItemList from './components/list-item/ItemList';
-const {Header, Content, Footer} = Layout;
+const { Header, Content, Footer } = Layout;
 
 class App extends React.Component {
   render() {
     return (
-    <div>
-      <Layout className="layout">
-        <HeaderBar/>
-        <Content style={{
-          padding: '0 50px'
-        }}>
-          <div
-            style={{
-            background: '#fff',
-            padding: 24,
-            minHeight: 280
+      <div>
+        <Layout className="layout">
+          <HeaderBar />
+          <Content style={{
+            padding: '0 50px'
           }}>
-            <ItemList/>
-          </div>
-        </Content>
-        <Footer style={{
-          textAlign: 'center'
-        }}>React Redux Workshop ©2012-2021 Created by Nextflow.in.th</Footer>
-      </Layout>,
-    </div>
+            <div
+              style={{
+                background: '#fff',
+                padding: 24,
+                minHeight: 280
+              }}>
+
+              <Row gutter={16}>
+                <Col span={20}></Col>
+                <Col span={4}>
+                  <Button type="primary" size="large" block>Add Item</Button>
+                </Col>
+              </Row>
+              <Divider/>
+              <Row gutter={16}>
+                <Col span={24}><ItemList /></Col>
+              </Row>
+              
+            </div>
+          </Content>
+          <Footer style={{
+            textAlign: 'center'
+          }}>React Redux Workshop ©2012-2021 Created by Nextflow.in.th</Footer>
+        </Layout>,
+      </div>
     );
   }
 }
