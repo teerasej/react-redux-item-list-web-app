e9894bf18dea83b89ff3523863f51ab61938e76d
Teerasej <teerasej@gmail.com>
2020-11-30 17:20:05 +0700

    create UI for new Item Form

--- src/components/new-item/NewItemForm.js
+++ src/components/new-item/NewItemForm.js
@@ -1,9 +1,51 @@
 import React from 'react'
+import { Form, Input, Button, Select } from 'antd';
+
+const { Option } = Select;
 
 export default function NewItemForm() {
+
+    const layout = {
+        labelCol: { span: 8 },
+        wrapperCol: { span: 16 },
+    };
+    const tailLayout = {
+        wrapperCol: { offset: 8, span: 16 },
+    };
+
+    const [form] = Form.useForm();
+
+    const onFinish = values => {
+        console.log(values);
+    };
+
     return (
-        <div>
-            Form
+        <div
+            style={{
+                background: '#fff',
+                padding: 24,
+                width: 350
+            }}>
+            <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
+                <Form.Item name="name" label="Name" rules={[{ required: true }]}>
+                    <Input />
+                </Form.Item>
+                <Form.Item name="status" label="Status" rules={[{ required: true }]}>
+                    <Select placeholder="Status">
+                        <Option value="instock">In Stock</Option>
+                        <Option value="preorder">Pre-order</Option>
+                    </Select>
+                </Form.Item>
+                
+                <Form.Item {...tailLayout}>
+                    <Button type="primary" htmlType="submit">
+                        Create
+                    </Button>
+                    <Button htmlType="button">
+                        Cancel
+                    </Button>
+                </Form.Item>
+            </Form>
         </div>
     )
 }
