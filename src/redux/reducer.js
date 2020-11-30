bef4dd1c6f8a3c0fc745d19e32b6eb034a6e8b32
Teerasej <teerasej@gmail.com>
2020-11-30 17:39:20 +0700

    handle form data from action object

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,13 +1,15 @@
 import Action from './action' 
 
 const initialState = {
-
+    
 }
 
 export default (state = initialState, { type, payload }) => {
     switch (type) {
 
     case Action.CREATE_NEW_ITEM:
+        console.log('Creating new item... ')
+        console.log(payload)
         return { ...state, ...payload }
 
     default:
