c744ed98c92b55d72411255c8fa6ae71e10e2cf2
Teerasej <teerasej@gmail.com>
2020-12-02 21:08:00 +0700

    dispatch data from web api to redux

--- src/components/list-item/ItemList.js
+++ src/components/list-item/ItemList.js
@@ -1,18 +1,25 @@
 import { List, Typography } from 'antd';
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { useEffect } from 'react'
 import Axios from 'axios';
+import action from '../../redux/action';
 
 
 export default function ItemList() {
 
     const items = useSelector(state => state.items)
+    const dispatch = useDispatch()
 
     useEffect(() => {
         const loadMessage = async () => {
             const notes = await Axios.get('http://localhost:3010/notes')
             console.log('notes loaded:', notes.data);
 
+            dispatch({
+                type: action.MESSAGE_LOADED,
+                payload: notes.data
+            })
         }
         loadMessage();
     }, [])
